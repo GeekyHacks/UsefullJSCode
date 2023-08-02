fcd67d83c2fc5b42374d9cdf2d0a299871bc6444
Abdullah <86020057+GeekyHacks@users.noreply.github.com>
2023-08-02 17:37:26 +0300

    Add Async

--- async.js
+++ async.js
@@ -1,9 +1,15 @@
 // Basic Syntax
 async function myDisplay() {
-    let myPromise = new Promise(function(resolve, reject) {
-      resolve("I love You !!");
-    });
-    document.getElementById("demo").innerHTML = await myPromise;
-  }
-  
-  myDisplay();
+  let myPromise = new Promise((resolve, reject) => {
+    resolve('I love You !!');
+    // to make the function waite use the settimeOut
+    setTimeout(resolve('I love You !!'), 3000);
+  });
+  document.getElementById('demo').innerHTML = await myPromise;
+}
+
+// The two arguments (resolve and reject) are pre-defined by JavaScript.
+// We will not create them, but call one of them when the executor function is ready.
+// Very often we will not need a reject function.
+
+myDisplay();
